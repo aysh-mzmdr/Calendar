5c3765822d9bd72ff05833db7a8ef48eceba9262
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-04-03 13:12:25 +0530

    Set up Backend CORS and Routing

---
+++ server/index.mjs
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+import Router from "./router.mjs"
+import cors from "cors"
+
+dotenv.config({path:"../.env"})
+const SERVER_PORT=process.env.SERVER_PORT
+const CLIENT_PORT=process.env.CLIENT_PORT
+
+const app = express()
+
+app.use(cors({origin:`http://localhost:${CLIENT_PORT}`,credentials:true}))
+app.use(Router)
+
+app.listen(SERVER_PORT)
